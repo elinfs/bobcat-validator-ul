cae6e1f7efeaee97b42cfaccaae5306c017ac9db
Niklas Ederoth <niklas.ederoth@gmail.com>
2018-02-20 10:05:18 +0100

    added cronConf

---
+++ cronConf/cron_conf/main.py
@@ -0,0 +1,18 @@
+from azure.storage.blob import BlockBlobService
+
+
+def main():
+    try:
+        import cron_conf.config as config
+    except:
+        raise ValueError('Please specify configuration settings in config.py.')
+        
+    block_blob_service = BlockBlobService(account_name=config.STORAGE_ACCOUNT_NAME, account_key=config.STORAGE_ACCOUNT_KEY)
+
+    generator = block_blob_service.list_blobs(config.STORAGE_CONTAINER)
+    for blob in generator:
+        print(blob.name)
+        block_blob_service.get_blob_to_path(config.STORAGE_CONTAINER, blob.name, config.DYNAMIC_FILES_LOCAL_PATH + blob.name)
+
+if __name__ == "__main__":
+    main()
