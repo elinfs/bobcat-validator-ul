1b83e7ec85754cb28b77be8aa1367dae8af950f3
Niklas Ederoth <niklas.ederoth@gmail.com>
2018-03-01 13:55:09 +0100

    updated cornconf schemavalidation with debug logs

--- cronConf/cron_conf/main.py
+++ cronConf/cron_conf/main.py
@@ -1,8 +1,11 @@
+# -*- encoding: utf-8 -*-
 from azure.storage.blob import BlockBlobService
 import json
 import yaml
+import jsonschema
 import logging
 import argparse
+from cron_conf.schema_validator import SchemaValidator
 
 ALL_SCHEMAS = [    
     'blacklists',
@@ -28,18 +31,20 @@ class CronConf:
         
         block_blob_service = BlockBlobService(account_name=config.STORAGE_ACCOUNT_NAME, account_key=config.STORAGE_ACCOUNT_KEY)
 
-        generator = block_blob_service.list_blobs(config.STORAGE_CONTAINER)
-        for blob in generator:
-            print(blob.name)
+        blobs = block_blob_service.list_blobs(config.STORAGE_CONTAINER)
+        for blob in blobs:
+            self.logger.debug('Blob name: ' + blob.name)
             try:
-                data = json.load(block_blob_service.get_blob_to_text(config.STORAGE_CONTAINER, blob.name))
-                self.schema.validate(data, blob.name[:4])
-                with open(DYNAMIC_FILES_LOCAL_PATH + blob.name, 'w+') as file:
-                    file.write(json.dumps(data))
+                data = block_blob_service.get_blob_to_text(config.STORAGE_CONTAINER, blob.name)
+                self.schema.validate(json.load(data.content, encoding = 'utf-8'), blob.name[:-5])
+                self.logger.debug('Schema is ok for: ' + blob.name)
+                with open(config.DYNAMIC_FILES_LOCAL_PATH + blob.name, 'w+', encoding = 'utf-8') as file:
+                    file.write(data.content)
+                    self.logger.debug('File updated: ' + config.STORAGE_CONTAINER + blob.name)
             except json.decoder.JSONDecodeError:
-                self.logger.critical("Error loading JSON data from %s", filename)            
+                self.logger.critical("Error loading JSON data from %s", blob.name)            
             except jsonschema.exceptions.ValidationError as e:
-                self.logger.critical("Syntax error parsing %s: %s", filename, e.message)                
+                self.logger.critical("Syntax error parsing %s: %s", blob.name, e.message)                
             except Exception as e:
                 self.logger.critical("error reading json blob: " + blob.name + " | exception: " + str(e))
 
